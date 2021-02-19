560071ffb8ecc9f421274e6928d6ca272a52da9e
andreas-marccain <andreas.schulze@marc-cain.de>
2021-02-19 14:17:53 +0100

    initial commit

---
+++ source/DebugExampleProxy/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace DebugExampleProxy
+{
+    using Castle.DynamicProxy;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start!");
+            var generator = new ProxyGenerator();
+
+            var proxy = generator.CreateInterfaceProxyWithTarget<IFoo>(new Target(),new Interceptor());
+
+            /* 1: */ proxy.Method();
+
+        }
+    }
+
+    class Interceptor : IInterceptor
+
+    {
+
+        public void Intercept(IInvocation invocation)
+
+            /* 2: */
+        {
+            Console.WriteLine("In interceptor");
+            /* 3: */
+            invocation.Proceed();
+
+        }
+
+    }
+
+    public interface IFoo
+    {
+        void Method();
+    }
+    class Target : IFoo
+
+    {
+
+        public void Method()
+
+            /* 4: */
+        {
+            Console.WriteLine("In target");
+        }
+
+    }
+
+}
